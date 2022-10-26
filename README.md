22934a62b919d522704ef0321e07c018eb1b0f71
frappe <frappe@ubuntu>
2022-10-26 23:38:37 +0000

    feat: Initialize App

---
+++ README.md
@@ -0,0 +1,7 @@
+## Library Management App
+
+Library Management App
+
+#### License
+
+MIT
---
+++ libraryms/config/desktop.py
@@ -0,0 +1,12 @@
+from frappe import _
+
+def get_data():
+	return [
+		{
+			"module_name": "Library Management App",
+			"color": "black",
+			"icon": "octicon octicon-file-directory",
+			"type": "module",
+			"label": _("Library Management App")
+		}
+	]
